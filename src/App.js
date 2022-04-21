58409181f5e4fce1d4f19e4f5143b63d347d8656
Zak Mayfield <zmayfield@parkbenchsolutions.com>
2022-04-21 13:24:00 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import * as React from 'react';
+// import { ethers } from 'ethers';
 import './App.css';
 
-function App() {
+export default function App() {
+  const wave = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className='mainContainer'>
+      <div className='dataContainer'>
+        <div className='header'>👋 Hey there!</div>
+
+        <div className='bio'>
+          I am farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className='waveButton' onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
